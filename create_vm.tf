cbfdbc874ecd97fd61a79abdfab19989bac7fe81
Fabiano Alves Pereira <fabiano341@hotmail.com>
2020-12-11 15:26:43 -0300

    remocao!!!!

--- create_vm.tf
+++ create_vm.tf
@@ -1,4 +1,4 @@
-# Create a virtual network!!!!!!!!
+# Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
     name                = "myTFVnet"
     address_space       = ["10.0.0.0/16"]
