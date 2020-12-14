aba327325937d3d5ff0c1f8c63015704852d99ea
Fabiano Alves Pereira <fabiano341@hotmail.com>
2020-12-14 14:17:15 -0300

    add file var

---
+++ var.tf
@@ -0,0 +1,11 @@
+variable "admin_username"{
+    type = string
+    description = "VM user name"
+    default = "fabiano"
+}
+
+variable "admin_password"{
+    type = string
+    description = "user password for vm"
+    defaul = "Palmeiras27#"
+}
